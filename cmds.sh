4a697cbe7b7587e28d3d05305c17a7b495ec6ca4
Aman <amanme9285@gmail.com>
2026-02-20 07:27:31 +0530

    Update cmds.sh

--- cmds.sh
+++ cmds.sh
@@ -6,12 +6,17 @@ OLD_VENDOR="yaap"
 MANIFEST_URL=https://github.com/LineageOS/android_manifest.git
 BRANCH=16.2
 
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
+ccache -M 50G
+ccache -o compression=true
+
 mkdir -p ~/Code/$ROM
 cd ~/Code/$ROM
 
-repo init -u $MANIFEST_URL -b $BRANCH --git-lfs
+repo init -u $MANIFEST_URL -b $BRANCH --git-lfs --depth=1
 git clone https://github.com/kagiyanagi/local_manifests.git .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --retry-fetches=25 --prune
 
 cd device/xiaomi/gale
 
@@ -25,10 +30,12 @@ sed -i "s/${OLD_VENDOR}_gale/${ROM}_gale/g" "${ROM}_gale.mk"
 
 cd ../../../
 
-source build/envsetup.sh
-lunch ${ROM}_gale-bp2a-userdebug
+# now setup rbe
 
-mka bacon  -j$(nproc --all
+source build/envsetup.sh
+lunch ${ROM}_gale-$(find $(gettop)/build/release/aconfig/* -maxdepth 0 -type d -name "[a-z][a-z][0-9][a-z]" -printf '%f\n' | tail -n1)-userdebug
+
+mka bacon -j$(nproc --all)
 # mka clean
 # mka cleaninstall
 # rm -rf out
