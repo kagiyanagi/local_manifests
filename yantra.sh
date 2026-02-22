a1ade9a91388f70aba49e80e8f3e3008f63638e0
Aman <amanme9285@gmail.com>
2026-02-22 17:38:25 +0530

    Refactor yantra.sh for improved readability

--- yantra.sh
+++ yantra.sh
@@ -2,8 +2,8 @@
 set -e
 
 ROM="cipher"
-MANIFEST_URL=https://github.com/CipherOS/android_manifest.git
-BRANCH=sixteen-qpr2
+MANIFEST_URL="https://github.com/CipherOS/android_manifest.git"
+BRANCH="sixteen-qpr2"
 
 export BUILD_USERNAME="Ren"
 export BUILD_HOSTNAME="Kagiyanagi"
@@ -19,7 +19,7 @@ export BUILD_HOSTNAME="Kagiyanagi"
 
 rm -rf .repo/local_manifests
 
-repo init -u $MANIFEST_URL -b $BRANCH --git-lfs --depth=1
+repo init -u "$MANIFEST_URL" -b "$BRANCH" --git-lfs --depth=1
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --retry-fetches=25 --prune
 
 git clone https://github.com/xaveroprjkt/device_xiaomi_gale.git device/xiaomi/gale -b lineage-23.2 --depth=1
@@ -39,7 +39,7 @@ if [ ! -d "device/${ROM}/sepolicy/libperfmgr" ]; then
     mv "$TMP_DIR/libperfmgr" "device/${ROM}/sepolicy/"
     rm -rf "$TMP_DIR"
     echo "libperfmgr moved successfully."
-    sed -i "2s|^[[:space:]]*device/lineage/sepolicy/libperfmgr/vendor$|    device/${ROM}/sepolicy/libperfmgr/vendor|" device/"${ROM}"/sepolicy/libperfmgr/sepolicy.mk
+    sed -i "2s|^[[:space:]]*device/lineage/sepolicy/libperfmgr/vendor$|    device/${ROM}/sepolicy/libperfmgr/vendor|" "device/${ROM}/sepolicy/libperfmgr/sepolicy.mk"
 else
     echo "device/${ROM}/sepolicy/libperfmgr already exists. Skipping."
 fi
@@ -51,15 +51,13 @@ mv "lineage.dependencies" "${ROM}.dependencies"
 mv "lineage_gale.mk" "${ROM}_gale.mk"
 
 sed -i "s/lineage_gale.mk/${ROM}_gale.mk/g" AndroidProducts.mk
-sed -i "s/vendor\/lineage/vendor\/${ROM}/g" BoardConfig.mk
-sed -i "s/vendor\/lineage/vendor\/${ROM}/g" "${ROM}_gale.mk"
+sed -i "s|vendor/lineage|vendor/${ROM}|g" BoardConfig.mk
+sed -i "s|vendor/lineage|vendor/${ROM}|g" "${ROM}_gale.mk"
 sed -i "s/lineage_gale/${ROM}_gale/g" "${ROM}_gale.mk"
 
 cd ../../../
 
-# now setup rbe
-
 source build/envsetup.sh
-lunch ${ROM}_gale-$(find $(gettop)/build/release/aconfig/* -maxdepth 0 -type d -name "[a-z][a-z][0-9][a-z]" -printf '%f\n' | tail -n1)-userdebug
+lunch ${ROM}_gale-$(find "$(gettop)/build/release/aconfig" -maxdepth 1 -mindepth 1 -type d -name "[a-z][a-z][0-9][a-z]" -printf '%f\n' | tail -n1)-userdebug
 make installclean
 m bacon -j$(nproc --all)
